207aa8b0c318c2be83a7d1d24ce019f4744a5a57
Sagar Sumit <sagarsumit09@gmail.com>
2025-06-12 01:18:23 +0000

    [core] Investigate flaky test_ray_shutdown
    Signed-off-by: Sagar Sumit <sagarsumit09@gmail.com>

--- python/ray/tests/test_ray_shutdown.py
+++ python/ray/tests/test_ray_shutdown.py
@@ -287,19 +287,16 @@ def test_raylet_graceful_exit_upon_agent_exit(ray_start_cluster):
     raylet, agent = get_raylet_agent_procs(worker)
     agent.terminate()
     exit_code = raylet.wait()
-    # When the agent is terminated
+    # When the agent is terminated gracefully, raylet should exit gracefully.
     assert exit_code == 0
 
-    # Make sure raylet exits gracefully upon agent terminated by SIGKILL.
-    # TODO(sang): Make raylet exits ungracefully in this case. It is currently
-    # not possible because we cannot detect the exit code of children process
-    # from cpp code.
+    # Make sure raylet exits ungracefully upon agent terminated by SIGKILL.
     worker = cluster.add_node(num_cpus=0)
     raylet, agent = get_raylet_agent_procs(worker)
     agent.kill()
     exit_code = raylet.wait()
-    # When the agent is terminated
-    assert exit_code == 0
+    # When the agent is killed abruptly, raylet should exit ungracefully.
+    assert exit_code != 0
 
 
 def test_raylet_graceful_exit_upon_runtime_env_agent_exit(ray_start_cluster):
